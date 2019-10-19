4a0ea9e2e219979950c8c8783e63fe025b59aed4
moises dj skip <15460643@itcolima.edu.mx>
2019-10-19 11:08:25 -0500

    Codigos

---
+++ Funciones.py
@@ -0,0 +1,18 @@
+"""
+Autor: Moises Preciado
+Objetivo: Primer programa en python
+Nombre: Funcion.py
+"""
+#---------------------------------------------------------------
+# Funcio para invocar mensaje
+#---------------------------------------------------------------
+
+def printMensaje():
+    print("Funcion que no retorna nada")
+
+def main():
+    #invocamos funcion printMensaje
+    printMensaje()
+
+if __name__ == "__main__":
+    main()
---
+++ HolaMundo.py
@@ -0,0 +1,6 @@
+"""
+Autor: Moises Preciado
+Objetivo: Primer programa en python
+Nombre: HolaMundo.py
+"""
+print("Hola Mundo WE")
